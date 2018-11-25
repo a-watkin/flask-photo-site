d5ec8c67ab146eb2dfd9b8f003fabb17114d3bdf
adam <atomicpenguines@gmail.com>
2018-11-25 01:49:02 +0000

    added some templates, currently adding login

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, session, flash, redirect, url_for, g
+
+from functools import wraps
 
 
 from database_interface import Database
@@ -6,6 +8,13 @@ from database_interface import Database
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
+SECRET_KEY = 'secret'
+
 
 db = Database('eigi-data.db')
 
@@ -19,18 +28,29 @@ $ flask run
 """
 
 
-@app.route('/')
-def country_question():
-    return render_template("base.html")
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    status_code = 200
+    if request.method == 'POST':
+        try:
+            if request.form['username'] != app.config['USERNAME'] or \
+                    request.form['password'] != app.config['PASSWORD']:
+                error = 'Invalid Credentials. Please try again.'
+                status_code = 401
+            else:
+                session['logged_in'] = True
+                return redirect(url_for('main'))
+
+        except Exception as e:
+            print('error ', e)
+
+    # if method is get then this is returned
+    return render_template('login.html', error=error), status_code
 
 
-@app.route("/name/<name>")
-def index(name):
-    if name.lower() == "michael":
-        return "Hello, {}".format(name), 200
-    else:
-        return "Not Found", 404
-
+# with app.test_request_context():
+#     print(url_for('login'))
 
 # @app.route("/")
 # def hello():
@@ -40,5 +60,7 @@ def index(name):
 #     }
 
 #     return jsonify(tag_data)
+
+
 if __name__ == '__main__':
     app.run()
