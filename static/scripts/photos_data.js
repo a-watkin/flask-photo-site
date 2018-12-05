23ca48a68fb1d286d84e2c37b7eb5ba6be75f606
adam <atomicpenguines@gmail.com>
2018-12-05 07:08:28 +0000

    some styling and code cleanup

--- static/scripts/photos_data.js
+++ static/scripts/photos_data.js
@@ -127,11 +127,6 @@ class PhotosData extends React.Component {
         `http://127.0.0.1:5000/albums/${this.state.albumId}`
       );
     });
-
-    // console.log(`http://127.0.0.1:5000/albums/${this.state.albumId}`);
-    // window.location.assign(
-    // `http://127.0.0.1:5000/albums/${this.state.albumId}`
-    // });
   }
 
   photoClick(photo_id) {
