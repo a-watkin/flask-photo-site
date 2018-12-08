7ff8301e52f9a12ad1e9cdf1f10a253a3d3bd6f1
adam <atomicpenguines@gmail.com>
2018-12-08 15:29:47 +0000

    starting file structure for uploads

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def upload_file():
 
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
+            flash('No file selected')
             return redirect(request.url)
 
         # THIS DOESN'T WORK
