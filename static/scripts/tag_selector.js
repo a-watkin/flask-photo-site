4e4f9700889a96ab1b3dfc7c5f5c72db70c75cee
adam <atomicpenguines@gmail.com>
2018-12-13 06:40:09 +0000

    got selected tag in state

--- static/scripts/tag_selector.js
+++ static/scripts/tag_selector.js
@@ -7,7 +7,9 @@ class TagSelector extends React.Component {
     super(props);
     this.state = {
       isLoaded: false,
-      photoData: null,
+      original: null,
+      title: null,
+      tags: null,
       selectedTags: [],
       albumId: null
     };
@@ -16,7 +18,7 @@ class TagSelector extends React.Component {
   }
 
   componentWillMount() {
-    console.log("hello from componentWillMount");
+    // console.log("hello from componentWillMount");
     // getting the album id from the URL
     let currentUrl = window.location.href;
     let splitUrl = currentUrl.split("=");
@@ -27,7 +29,7 @@ class TagSelector extends React.Component {
       .then(
         result => {
           // ok i am getting the data
-          console.log(`result ${result}`, Object.keys(result), result.original);
+          // console.log(`result ${result}`, Object.keys(result), result.original);
           this.setState({
             isLoaded: true,
             original: result.original,
@@ -46,60 +48,28 @@ class TagSelector extends React.Component {
       );
   }
 
-  sendData() {
-    if (this.state.selectedAlbum.length < 1) {
-      console.log("do nothing if no album has been selected");
-      return false;
-    }
-
-    console.log("getting here?", this.state.selectedAlbum);
-    fetch("http://127.0.0.1:5000/api/getalbums", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        albumId: this.state.selectedAlbum
-      })
-    }).then(() => {
-      console.log("eh");
-      // redirect after successful post
-      window.location.assign(
-        `http://127.0.0.1:5000/albums/${this.state.selectedAlbum[0]}`
-      );
-    });
-  }
-
-  tagClick(album_id) {
-    console.log("Greetings from albumClick the album_id is ", album_id);
-    console.log(this.state.selectedAlbum);
-
-    if (
-      this.state.selectedAlbum.length === 1 &&
-      this.state.selectedAlbum[0] === album_id
-    ) {
-      console.log("deselect it?");
-      let tempArray = [...this.state.selectedAlbum];
-      tempArray.splice(0, 1);
-      console.log(tempArray);
-      this.setState({
-        selectedAlbum: tempArray
-      });
-
-      console.log(this.state.selectedAlbum);
+  tagClick(tagName) {
+    console.log("clicked", tagName);
+    let tempTags = [...this.state.selectedTags];
+    console.log(tempTags);
+    if (tempTags.indexOf(tagName) === -1) {
+      tempTags.push(tagName);
     } else {
-      let tempArray = [];
-      tempArray.push(album_id);
-
-      // i only wan this to allow one item in the array
-      this.setState({
-        selectedAlbum: tempArray
-      });
+      console.log("getting here on first try?");
+      tempTags.splice(tempTags.indexOf(tagName), 1);
     }
+    console.log(
+      "should be altering state here",
+      tempTags,
+      this.state.selectedTags
+    );
 
-    // also not ideal but good enough for now
-    this.forceUpdate();
+    this.setState({
+      selectedTags: tempTags
+    });
+    // so for some reason it's just not updated yet
+    // when you try and get the state here?
+    console.log("what", this.state.selectedTags);
   }
 
   render() {
@@ -119,7 +89,27 @@ class TagSelector extends React.Component {
       color: "white"
     };
 
+    console.log("what is it here?", this.state.selectedTags);
+
     if (this.state.isLoaded) {
+      const tagData = this.state.tags;
+
+      const tag = tagData.map((tag, index) => {
+        console.log(tag, index);
+        const tagName = tag["tag_name"];
+        return (
+          <div key={index}>
+            <button
+              type="button"
+              className="btn btn-outline-success btn-lg"
+              onClick={() => this.tagClick(tagName)}
+            >
+              {tagName}
+            </button>
+          </div>
+        );
+      });
+
       return (
         <div>
           <div className="row text-center">
@@ -138,6 +128,7 @@ class TagSelector extends React.Component {
               </h3>
               <p>Select tags to remove by clicking on them below.</p>
               <hr />
+              {tag}
             </div>
           </div>
           <hr />
