85596ca75a2a47be4e80c00ce3b4901cef5505c5
adam <atomicpenguines@gmail.com>
2018-12-18 16:55:02 +0000

    got 300x300 for all photos and from local files

--- photo_path_update.py
+++ photo_path_update.py
@@ -3,37 +3,72 @@ import datetime
 
 
 from database_interface import Database
+import resize_photo
 
 db = Database('eigi-data.db')
-# read date taken for the photo
-# 30081941117
 
-date_taken = db.make_query(
+photo_ids = db.make_query(
     '''
-    select date_taken from photo where photo_id = '30081941117'
+    select photo_id from photo
     '''
 )
 
+# print(photo_ids)
 
-filename = db.make_query(
-    '''
-    select original from images where photo_id = '30081941117'
-    '''
-)
-split_filename = filename[0][0].split('/')
-filename = split_filename[len(split_filename) - 1]
+for photo in photo_ids:
+    print()
+    photo_id = photo[0]
 
-print(filename)
+    # read date taken for the photo
+    # 30081941117
 
-# 2016-07-31 15:53:03
-dt_obj = datetime.datetime.strptime("2016-07-31 15:53:03", "%Y-%m-%d %H:%M:%S")
-print(dt_obj.hour)
+    date_uploaded = db.make_query(
+        '''
+        select date_uploaded from photo where photo_id = "{}"
+        '''.format(photo_id)
+    )
 
+    date_uploaded = date_uploaded[0][0]
 
-original_path = '/static/images/{}/{}/{}'.format(
-    dt_obj.year, dt_obj.month, filename)
+    dt_obj = datetime.datetime.fromtimestamp(int(date_uploaded))
 
-# large_square_path = '/static/images/{}/{}/{}'.format(
-#     dt_obj.year, dt_obj.month, thumbnail_filename)
+    filename = db.make_query(
+        '''
+        select original, large_square from images where photo_id = "{}"
+        '''.format(photo_id)
+    )
+    split_filename = filename[0][0].split('/')
+    filename_filename = split_filename[len(split_filename) - 1]
 
-print(os.getcwd() + original_path)
+    split_filename = filename[0][1].split('/')
+    large_square_filename = split_filename[len(split_filename) - 1]
+
+    # print(filename)
+
+    # # 2016-07-31 15:53:03
+    # dt_obj = datetime.datetime.strptime("2016-07-31 15:53:03", "%Y-%m-%d %H:%M:%S")
+    # print(dt_obj.hour)
+
+    original_path = '/static/images/{}/{}/{}'.format(
+        dt_obj.year, dt_obj.month, filename_filename)
+
+    large_square_path = '/static/images/{}/{}/{}'.format(
+        dt_obj.year, dt_obj.month, large_square_filename)
+
+    print(original_path)
+    print(os.getcwd() + original_path)
+    print(os.getcwd() + large_square_path)
+
+    large_square_dir = '/static/images/{}/{}/'.format(
+        dt_obj.year, dt_obj.month)
+
+    resize_photo.square_thumbnail(
+        os.getcwd() + original_path, large_square_filename, os.getcwd() + large_square_dir, 300)
+
+    db.make_query(
+        '''
+        update images
+        set large_square = "{}"
+        where photo_id = "{}"
+        '''.format(large_square_path, photo_id)
+    )
