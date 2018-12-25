171ffd7d197a047d05cfbb0792034d2dafa0b189
adam <atomicpenguines@gmail.com>
2018-12-25 11:51:56 +0000

    add an admin account to the db

---
+++ admin.py
@@ -0,0 +1,27 @@
+from database_interface import Database
+from password_util import PasswordUtil
+
+
+class Admin(object):
+    def __init__(self):
+        self.db = Database('eigi-data.db')
+
+    def make_account(self, username, password):
+        self.db.make_query(
+            '''
+            insert into user (user_id, username, hash_value)
+            values ("{}", "{}", "{}")
+            '''.format(
+                username,
+                username,
+                PasswordUtil.hash_password(password))
+        )
+
+
+def main():
+    a = Admin()
+    a.make_account('', '')
+
+
+if __name__ == "__main__":
+    main()
