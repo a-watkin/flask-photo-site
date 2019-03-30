73532b50c6f7771bc57263cc985a3713b3918374
A-Watkin <awatkin.work@gmail.com>
2019-03-30 02:40:42 +0000

    fixed a typo

--- user/user.py
+++ user/user.py
@@ -35,7 +35,7 @@ class User(object):
 
     def get_hashed_password(self, username):
         """
-        Returns the hasehd password from the database for the given username.
+        Returns the hashed password from the database for the given username.
         """
         db_resp = self.db.get_row('user', 'username', self.username)
         return db_resp[2]
