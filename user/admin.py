338dde253d6e5a6d00cd2c4c836af77f081b8703
A-Watkin <awatkin.work@gmail.com>
2019-04-03 21:31:34 +0100

    removed database name

--- user/admin.py
+++ user/admin.py
@@ -4,7 +4,7 @@ from password_util import PasswordUtil
 
 class Admin(object):
     def __init__(self):
-        self.db = Database('eigi-data.db')
+        self.db = Database()
 
     def make_account(self, username, password):
         self.db.make_query(
