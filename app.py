2acf731ca04d4901d9c3ac816ca6a4960c4ab7c1
A-Watkin <awatkin.work@gmail.com>
2019-04-05 02:21:34 +0100

    changed app start up, added photo prefix to upload and album

--- app.py
+++ app.py
@@ -9,22 +9,24 @@ from album.album_routes import album_blueprint
 from photo_tag.photo_tag_routes import photo_tag_blueprint
 
 
-app = Flask('app')
-app = Flask(__name__.split('.')[0])
+# app = Flask('app')
+# app = Flask(__name__.split('.')[0])
+app = Flask(__name__)
 
 # Apply config values.
 app.config.from_object('config.DevelopmentConfig')
 
+
 # Register blueprints.
 app.register_blueprint(photo_blueprint, url_prefix="/photo")
 app.register_blueprint(user_blueprint, url_prefix="/photo/user")
-app.register_blueprint(upload_blueprint, url_prefix="/upload")
-app.register_blueprint(album_blueprint, url_prefix="/album")
+app.register_blueprint(upload_blueprint, url_prefix="/photo/upload")
+app.register_blueprint(album_blueprint, url_prefix="/photo/album")
 app.register_blueprint(photo_tag_blueprint, url_prefix="/photo/tag")
 
 
-if __name__ == '__main__':
-    app.run(
-        host='0.0.0.0',
-        debug=True
-    )
+# if __name__ == '__main__':
+#     app.run(
+#         host='0.0.0.0',
+#         debug=True
+#     )
